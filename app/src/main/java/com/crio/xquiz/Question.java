35b7869a09e072ed3c9bf0d5037c9780df713ffd
CrioUser <crio-user@criodo.com>
2023-07-13 00:34:05 +0530

    ME_BUILDOUT_XQUIZ_MODULE_PROJECT

--- app/src/main/java/com/crio/xquiz/Question.java
+++ app/src/main/java/com/crio/xquiz/Question.java
@@ -3,12 +3,30 @@ import java.util.*;
 
 public class Question{
     //TODO: Define the variable to store the question text
+    String questionText;
     //TODO: Define the variable to store the answer text
+    String answer;
     //TODO: Define the variable to store the list of text choices
-
-    // TODO: Create the Question class constructor 
+       
+        List<String> choices;
+        // TODO: Create the Question class constructor 
     // use the  signature :  public Question(String questionText, List<String> choices, String answer)
     // Include the following validations
+    public Question(String questionText,List<String> choices,String answer){
+        if(questionText==null || questionText == "")
+                System.out.println("Question text cannot be null or empty!");
+        else if(choices.size()<=0)
+                System.out.println("Choices cannot be null or empty!");
+        else if(answer==null || answer=="")
+                System.out.println("Answer cannot be null or empty!");
+        else if(choices.contains(answer)==false)
+                System.out.println("Answer is not present among the choices!");
+        else{
+                this.questionText = questionText;
+                this.answer = answer;
+                this.choices = choices;
+        }
+    }
 
         // Validate: If QuestionText is  null or empty
                 // Print "Question text cannot be null or empty!"
@@ -26,14 +44,29 @@ public class Question{
     // If the print statements do not match then the tests might fail, so be cautious
 
     //TODO: Create the getter method : public String getAnswer(), which returns the answer
+    public String getAnswer(){
+        return answer;
+    }
 
     //TODO: Create the getter method : public String getQuestionText(), which returns the questionText
+    public String getQuestionText(){
+        return questionText;
+    }
 
     //TODO: Create the getter method : public List<String> getChoices(), which returns the choices
+    public List<String> getChoices(){
+        return choices;
+    }
 
     //TODO: Create the method checkAnswer() with signature: public boolean checkAnswer(String answer)
         // Validate: If answer provided in the input matches the existing answer for the given question, then return True
         // Else:  return False.
+    public boolean checkAnswer(String answer){
+        if(this.answer == answer)
+                return true;
+        else
+                return false;
+    }
 
 
 public void display(){
